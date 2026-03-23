a6122da9b494fcfa9a657e059ed44aafa5d2674f
vladislaav <parieiko.vlad@gmail.com>
2026-03-23 12:29:27 +0400

    Update views

--- library/urls.py
+++ library/urls.py
@@ -18,19 +18,13 @@ from django.contrib import admin
 from django.urls import path
 
 from my_app.views import (
-    get_all_books,
-    get_book_by_id,
-    create_new_book,
-    update_book,
-    delete_book
+    list_create_books,
+    retrieve_update_destroy_book,
 )
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', get_all_books),
-    path('books/create/', create_new_book),
-    path('books/<int:pk>', get_book_by_id),
-    path('books/<int:pk>/update/', update_book),
-    path('books/<int:pk>/delete/', delete_book),
+    path('books/', list_create_books),
+    path('books/<int:pk>', retrieve_update_destroy_book),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -10,24 +10,39 @@ from my_app.serializers import BooksSerializer, BookCreateSerializer, BookUpdate
 
 
 # получить список всех книг
-@api_view(['GET'])
-def get_all_books(request: Request) -> Response:
-    # 1. Получить объекты из БД
-    queryset: QuerySet[Book] = Book.objects.all()
+@api_view(['GET', 'POST'])
+def list_create_books(request: Request) -> Response:
+    if request.method == 'GET':
+        # 1. Получить объекты из БД
+        queryset: QuerySet[Book] = Book.objects.all()
 
-    # 2. Преобюразовать в простые объекты
-    serializer = BooksSerializer(queryset, many=True)  # QuerySet -> [dict[]]
+        # 2. Преобюразовать в простые объекты
+        serializer = BooksSerializer(queryset, many=True)  # QuerySet -> [dict[]]
 
-    # 3. Вернуть результат
-    return Response(
-        data=serializer.data,
-        status=status.HTTP_200_OK
-    )
+        # 3. Вернуть результат
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
+    elif request.method == 'POST':
+        try:
+            serializer = BookCreateSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+        except ValidationError as err:
+            return Response(
+                data={"error": f"Validation error: {err}"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_201_CREATED
+        )
 
 
-@api_view(['GET'])
-def get_book_by_id(request: Request, pk: int) -> Response:
-    # 1. Получить объекты из БД
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def retrieve_update_destroy_book(request: Request, pk: int) -> Response:
     try:
         obj: Book = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
@@ -36,69 +51,29 @@ def get_book_by_id(request: Request, pk: int) -> Response:
             status=status.HTTP_404_NOT_FOUND
         )
 
-    # 2. Преобюразовать в простые объекты
-    serializer = BooksSerializer(obj)  # QuerySet -> [dict[]]
+    if request.method == 'GET':
+        serializer = BooksSerializer(obj)  # QuerySet -> [dict[]]
 
-    # 3. Вернуть результат
-    return Response(
-        data=serializer.data,
-        status=status.HTTP_200_OK
-    )
+        # 3. Вернуть результат
+        return Response(
+            data=serializer.data,
+            status=status.HTTP_200_OK
+        )
+    elif request.method in ['PUT', 'PATCH']:
+        partial = False if request.method == 'PUT' else True
 
-
-@api_view(['POST'])
-def create_new_book(request: Request) -> Response:
-    try:
-        serializer = BookCreateSerializer(data=request.data)
+        serializer = BookUpdateSerializer(instance=obj, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-    except ValidationError as err:
+
         return Response(
-            data={"error": f"Validation error: {err}"},
-            status=status.HTTP_400_BAD_REQUEST
+            data=serializer.data,
+            status=status.HTTP_200_OK
         )
+    elif request.method == 'DELETE':
+        obj.delete()
 
-    return Response(
-        data=serializer.data,
-        status=status.HTTP_201_CREATED
-    )
-
-
-@api_view(['PUT', 'PATCH'])
-def update_book(request: Request, pk: int) -> Response:
-    try:
-        obj = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
         return Response(
-            data={"error": f"Book with ID {pk} does not exist"},
-            status=status.HTTP_404_NOT_FOUND
+            data={"message": f"Book with ID {pk} deleted successfully"},
+            status=status.HTTP_204_NO_CONTENT
         )
-
-    partial = False if request.method == 'PUT' else True
-
-    serializer = BookUpdateSerializer(instance=obj, data=request.data, partial=partial)
-    serializer.is_valid(raise_exception=True)
-    serializer.save()
-
-    return Response(
-        data=serializer.data,
-        status=status.HTTP_200_OK
-    )
-
-
-@api_view(['PUT', 'PATCH'])
-def delete_book(request: Request, pk: int) -> Response:
-    try:
-        obj = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        return Response(
-            data={"error": f"Book with ID {pk} does not exist"},
-            status=status.HTTP_404_NOT_FOUND
-        )
-
-    obj.delete()
-
-    return Response(
-        data={"message": f"Book with ID {pk} deleted successfully"},
-        status=status.HTTP_204_NO_CONTENT
-    )
